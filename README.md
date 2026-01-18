0a5de5210a273a0bb7e32cf08a1b0406ed278207
Pratik <pratik216029@gmail.com>
2026-01-19 00:09:07 +0530

    19 jan

--- README.md
+++
@@ -1,2 +0,0 @@
-# Problem-Solving
-This is a repository which contains daily one problem to solve and push the code on github.
--- practice/a.java
+++ practice/a.java
@@ -12,7 +12,25 @@ public class a {
         }
         return min;
     }
+
+    public int findKRotation(int arr[]) {
+        int start=0,end=arr.length-1;
+        int ans=-1;
+        while(start<end){
+            int mid=start+(end-start)/2;
+            if(arr[mid]<arr[end]){
+                ans=Math.min(ans,mid);
+                end=mid-1;
+            }else if(arr[mid]>arr[end]){
+                start=mid+1;
+            }
+        }
+        return ans;
+    }
+
     public static void main(String[] args) {
-        System.out.println(new a().findMin(new int[]{4,5,6,7,0,1,2}));
+        // System.out.println(new a().findMin(new int[]{4,5,6,7,0,1,2}));
+        System.out.println(new aa().findKRotation(new int[]{5,1,2,3,4}));
+        
     }
 }
